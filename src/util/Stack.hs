012c1472311c5cb53156b0f861df96400e513f37
Patrick Yevych <patrick.yevych@mail.utoronto.ca>
2023-07-13 22:06:42 -0400

    add util.stack

---
+++ src/util/Stack.hs
@@ -0,0 +1,13 @@
+module Stack where
+
+newtype Stack a = Stack [a] deriving Show
+
+init :: Stack a
+init = Stack []
+
+push :: a -> Stack a -> Stack a
+push x (Stack xs) = Stack (x:xs)
+
+pop :: Stack a -> (Maybe a, Stack a)
+pop (Stack []) = (Nothing, Stack [])
+pop (Stack (x:xs)) = (Just x, Stack xs)
